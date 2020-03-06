bc268ad6de5adb91fee7740771d87c43c75518a4
Andre Barreto <52931189+andrelbarreto@users.noreply.github.com>
2020-03-05 21:39:34 -0600

    Made changes to server for Heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB ||"mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
